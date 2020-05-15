8b2fd0819787fec7158c57c52fd8634c8724725e
zhangcong <zhangcong@9linktech.com>
2020-05-15 14:00:33 +0800

    feat:week6作业2 kmp

---
+++ week06/kmp.js
@@ -0,0 +1,33 @@
+function match (pattern, string) {
+  let stateHandlers = []
+  for (const p of pattern) {
+    const len = stateHandlers.length
+    if(!len){
+      stateHandlers.push(
+        function start(c) {
+          if (c === p)
+            return stateHandlers[len+1]
+          else 
+            return start
+        }
+      )
+    }
+    else if (len < pattern.length) {
+      stateHandlers.push(
+        function (c) {
+          if (c === p)
+            return stateHandlers[len+1]
+          else 
+            return stateHandlers[0](c)
+        }
+      )
+    }
+  }
+  stateHandlers.push(function end(){return end})
+  let state = stateHandlers[0]
+  for (const c of string) {
+    state = state(c)
+  }
+  return state === stateHandlers[stateHandlers.length - 1]
+}
+
